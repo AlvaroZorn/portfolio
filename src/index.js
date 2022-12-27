c2e8c19886bc2c91c6f98743bb0d9afd6d71c48e
Alvaro Zorn <alvaro.zorn@cimt-ag.de>
2022-12-27 19:00:30 +0100

    AZ-1000: some changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import "./index.css";
 import "./i18n/config";
-
-import React, { Component } from "react";
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(
-    <App />
-  );
+root.render(<App />);
